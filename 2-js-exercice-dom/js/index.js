ebc054ae92dce3032821effb735b40652acf09c1
Faysal Driouach <fdriouach10@gmail.com>
2024-07-17 13:37:21 +0200

    commit 17/07/24

--- 2-js-exercice-dom/js/index.js
+++ 2-js-exercice-dom/js/index.js
@@ -1,1 +1,11 @@
-console.log("template ok");
+const body = document.querySelector('body')
+console.log(body);
+
+let header = document.createElement('header')
+console.log(header);
+
+let main = document.createElement('main')
+console.log(main);
+
+body.appendChild(header)
+body.appendChild(main)
